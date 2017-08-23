d44a9666a03c3dbc2bfa4d62a93ebb57bbefdce7
utpaul <utpaulsarkar@gmail.com>
2017-08-23 17:50:25 +0600

    details of problem update....

--- src/pages/details-problem/details-problem.ts
+++ src/pages/details-problem/details-problem.ts
@@ -12,10 +12,11 @@ export class DetailsProblemPage implements OnInit{
   private loadData:any;
   private imageUrl:any;
   private imagesList:Array<string>;
-
+  private recordedFileUrl:any;
   constructor(public navCtrl: NavController,
               public navParams: NavParams) {
     this.imageUrl =API_CONFIG.misBase2;
+    this.recordedFileUrl = API_CONFIG.misBase3;
   }
 
 
