869dac36de31b76aab6641ca67e77c7499c08a5b
utpaul <utpaulsarkar@gmail.com>
2017-08-23 17:51:32 +0600

    details list remove bug fixing

--- src/pages/details-list/details-list.ts
+++ src/pages/details-list/details-list.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import {IonicPage, NavParams, NavController} from 'ionic-angular';
 import {API_CONFIG} from "../../constants/api";
+import {L2nHttp} from "../../providers/l2n-http";
 
 @IonicPage()
 @Component({
@@ -13,12 +14,28 @@ export class DetailsListPage implements OnInit{
   private loadedList :any;
   private imageUrl:any;
 
-  constructor(public navParams: NavParams) {
+  constructor(public navParams: NavParams,
+              private l2nHttp:L2nHttp,
+              public navCtrl: NavController) {
     this.imageUrl =API_CONFIG.misBase;
   }
 
   ngOnInit(){
     this.loadedList = this.navParams.data;
+    console.log(this.loadedList);
+  }
+
+
+  removeTree(treeId){
+    this.l2nHttp.postRequest('selected-tree-remove',
+      {id:treeId}, 'তথ্য অপসারণ হচ্ছে...').then(
+      data => {
+        console.log('success');
+        this.navCtrl.pop();
+      }, err => {
+        console.log(err);
+      }
+    );
   }
 
 }
